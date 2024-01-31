c43f39561c33788ca2c406ebf60dd5ccf672315f
Rey <rsantelli@luc.edu>
2024-01-30 18:56:05 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// For the multiply requirement
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // This should equal 62
+
+// For the random requirement
+let random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+// For the mod requirement
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // Remainder of num3 divided by num4 (should be 4)
+
+// For the max requirement
+let max = Math.max(1, 3, 20, 5, 7); // This should return 20 as the highest number
